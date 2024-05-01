e126cdd9df9be19784e883b4efa62cd856d918d9
世盛 陈 <153713838@qq.com>
2024-05-01 10:57:48 +0800

    推送模块项目

---
+++ src/StockReview.Client.ContentModule/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StockReview.Client.ContentModule
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
