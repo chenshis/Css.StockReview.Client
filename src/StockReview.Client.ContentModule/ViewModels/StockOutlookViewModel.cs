32bf3f9d743447c07f9f03976c29893a71a64fd9
世盛 陈 <153713838@qq.com>
2024-05-04 10:45:59 +0800

    调整曲线

--- src/StockReview.Client.ContentModule/ViewModels/StockOutlookViewModel.cs
+++ src/StockReview.Client.ContentModule/ViewModels/StockOutlookViewModel.cs
@@ -108,6 +108,28 @@ namespace StockReview.Client.ContentModule.ViewModels
 
         public ISeries[] CandleSeries { get; set; }
 
+        public ISeries[] CurveSeries { get; set; }= 
+    {
+        new LineSeries<double>
+        {
+            Values = new double[] { 5, 0, 5, 0, 5, 0 },
+            Fill = null,
+            GeometrySize = 0,
+            // use the line smoothness property to control the curve
+            // it goes from 0 to 1
+            // where 0 is a straight line and 1 the most curved
+            LineSmoothness = 0.2
+        },
+        new LineSeries<double>
+        {
+            Values = new double[] { 7, 2, 7, 2, 7, 2 },
+            Fill = null,
+            GeometrySize = 0,
+            LineSmoothness = 1
+        }
+    };
+
+
 
     }
 
