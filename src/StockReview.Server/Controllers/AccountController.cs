3b37ab14218d4261000254bd2e4432428fb0831a
世盛 陈 <153713838@qq.com>
2024-05-07 15:32:57 +0800

    推送文件

--- src/StockReview.Server/Controllers/AccountController.cs
+++ src/StockReview.Server/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IdentityModel.Client;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StockReview.Api.Dtos;
 
@@ -12,8 +14,29 @@ namespace StockReview.Server.Controllers
 
         [HttpPost]
         [Route("v1/stock-review/account/login")]
-        public string Login([FromBody] AccountRequestDto accountRequest)
+        public async Task<string> Login([FromBody] AccountRequestDto accountRequest)
         {
+            var client = new HttpClient();
+            PasswordTokenRequest tokenRequest = new PasswordTokenRequest();
+            tokenRequest.Address = "http://localhost:5245/connect/token";
+            tokenRequest.GrantType = GrantType.ResourceOwnerPassword;
+            tokenRequest.ClientId = "Zhaoxi.AspNetCore31.AuthDemo";
+            tokenRequest.ClientSecret = "eleven123456";
+            tokenRequest.Scope = "TestApi";
+            tokenRequest.Parameters = new Dictionary<string, string>()
+            {
+                ["eMail"] = "57265177@qq.com",
+                ["role"] = "Admin",
+                [IdentityModel.JwtClaimTypes.NickName] = "Eleven"
+            };
+            tokenRequest.UserName = "Eleven";
+            tokenRequest.Password = "123456";
+
+
+            var tokenResponse = await client.RequestPasswordTokenAsync(tokenRequest);
+
+            var token = tokenResponse.AccessToken;
+            var tokenType = tokenResponse.TokenType;
             return "token";
         }
     }
--- src/StockReview.Server/Program.cs
+++ src/StockReview.Server/Program.cs
@@ -1,16 +1,45 @@
-using StockReview.Server;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.EntityFramework.Storage;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.EntityFrameworkCore;
 using StockReview.Server.Exceptions;
+using System.Reflection;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// assembly
+var migrationsAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 36));
+builder.Services.AddIdentityServer()
+    .AddDeveloperSigningCredential()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = builder =>
+        {
+            builder.UseMySql(connectionString, serverVersion, sql => sql.MigrationsAssembly(migrationsAssembly));
+        };
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = builder =>
+        {
+            builder.UseMySql(connectionString, serverVersion, sql => sql.MigrationsAssembly(migrationsAssembly));
+        };
+        options.EnableTokenCleanup = true;
+        options.TokenCleanupInterval = 3600;
+    })
+    .AddTestUsers(GetUsers());
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,8 +48,28 @@ if (app.Environment.IsDevelopment())
 }
 //异常中间件
 app.UseException();
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+static List<TestUser> GetUsers()
+{
+    return new List<TestUser>()
+            {
+                new TestUser()
+                {
+                     Username="Eleven",
+                     Password="123456",
+                     SubjectId="0",
+                     Claims=new List<Claim>(){
+                        new Claim(IdentityModel.JwtClaimTypes.Role,"Admin"),
+                        new Claim(IdentityModel.JwtClaimTypes.NickName,"Eleven"),
+                        new Claim("eMail","57265177@qq.com")
+                    }
+                }
+            };
+}
