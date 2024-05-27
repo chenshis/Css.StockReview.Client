d6186ff3f0a6bbcf3dc31cde084bdca6b300ca1c
962089443@qq.com <962089443@qq.com>
2024-05-27 16:34:38 +0800

    市场情绪

---
+++ src/StockReview.Client.ContentModule/ViewModels/MarketSentimentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace StockReview.Client.ContentModule.ViewModels
+{
+    public class MarketSentimentViewModel : NavigationAwareViewModelBase
+    {
+        public ObservableCollection<string> DateItem { get; set; } = new ObservableCollection<string>();
+
+        public MarketSentimentViewModel(IUnityContainer unityContainer, IRegionManager regionManager) : base(unityContainer, regionManager)
+        {
+            this.PageTitle = "市场情绪";
+
+            for (int i = 0; i < 5; i++)
+            {
+                DateItem.Add(DateTime.Now.AddYears(-i).Year.ToString());
+            }
+        }
+    }
+}
