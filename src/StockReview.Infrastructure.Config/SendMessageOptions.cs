b63b6566af108b97d9e35db960311f685daef797
世盛 陈 <153713838@qq.com>
2024-06-25 11:31:43 +0800

    增加短信配置参数

---
+++ src/StockReview.Infrastructure.Config/SendMessageOptions.cs
@@ -0,0 +1,54 @@
+﻿
+namespace StockReview.Infrastructure.Config
+{
+    /// <summary>
+    /// 发送消息选项
+    /// </summary>
+    public class SendMessageOptions
+    {
+        /// <summary>
+        /// secret
+        /// </summary>
+        public string AccessKeySecret { get; set; }
+        /// <summary>
+        /// key
+        /// </summary>
+        public string AccessKeyId { get; set; }
+
+        /// <summary>
+        /// 端口
+        /// </summary>
+        public string Endpoint { get; set; }
+
+        /// <summary>
+        /// 注册签名
+        /// </summary>
+        public string RegSignName { get; set; }
+
+        /// <summary>
+        /// 注册模板编号
+        /// </summary>
+        public string RegTemplateCode { get; set; }
+
+        /// <summary>
+        /// 忘记签名
+        /// </summary>
+        public string ForgotSignName { get; set; }
+
+        /// <summary>
+        /// 忘记模板编号
+        /// </summary>
+        public string ForgotTemplateCode { get; set; }
+
+        /// <summary>
+        /// 模板参数
+        /// </summary>
+
+        public string TemplateParam { get; set; }
+
+        /// <summary>
+        /// 每天限流次数
+        /// </summary>
+        public string Limit { get; set; }
+    }
+}
